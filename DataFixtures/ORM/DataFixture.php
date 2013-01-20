036b90218f4d4981fc707bcf4ea525766d7f9335
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2013-01-20 19:49:35 +0100

    Add basic taxation fixtures

---
+++ DataFixtures/ORM/DataFixture.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\CoreBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Faker\Factory as FakerFactory;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Base data fixture.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+abstract class DataFixture extends AbstractFixture implements ContainerAwareInterface, OrderedFixtureInterface
+{
+    /**
+     * Container.
+     *
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Faker.
+     *
+     * @var Generator
+     */
+    protected $faker;
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->faker = FakerFactory::create();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Get service by id.
+     *
+     * @param string $id
+     *
+     * @return object
+     */
+    protected function get($id)
+    {
+        return $this->container->get($id);
+    }
+}
