98ce42f7c092d15bf721805488e1668f80659847
umpirsky <umpirsky@gmail.com>
2013-01-23 13:41:26 +0100

    Added shipping

---
+++ Entity/Shipment.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\CoreBundle\Entity;
+
+use Sylius\Bundle\ShippingBundle\Entity\Shipment as BaseShipment;
+
+/**
+ * Shipment attached to order.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class Shipment extends BaseShipment
+{
+}
