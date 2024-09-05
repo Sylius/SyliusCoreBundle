36e06dbd1681a35e82c02e7ae98413ce9b33d703
Francis Hilaire <Prometee@users.noreply.github.com>
2024-09-05 12:47:49 +0300

    Move ShopBundle order_pay classes to CoreBundle

---
+++ OrderPay/Provider/PaymentRequestAfterPayResponseProvider.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\OrderPay\Provider;
+
+use Sylius\Bundle\CoreBundle\OrderPay\Handler\PaymentStateFlashHandlerInterface;
+use Sylius\Bundle\CoreBundle\PaymentRequest\Announcer\PaymentRequestAnnouncerInterface;
+use Sylius\Bundle\CoreBundle\PaymentRequest\Provider\ServiceProviderAwareProviderInterface;
+use Sylius\Bundle\ResourceBundle\Controller\RequestConfiguration;
+use Sylius\Component\Core\Model\PaymentInterface;
+use Sylius\Component\Payment\Factory\PaymentRequestFactoryInterface;
+use Sylius\Component\Payment\Model\PaymentRequestInterface;
+use Sylius\Component\Payment\Repository\PaymentRequestRepositoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Webmozart\Assert\Assert;
+
+final class PaymentRequestAfterPayResponseProvider implements AfterPayResponseProviderInterface
+{
+    /**
+     * @param PaymentRequestFactoryInterface<PaymentRequestInterface> $paymentRequestFactory
+     * @param PaymentRequestRepositoryInterface<PaymentRequestInterface> $paymentRequestRepository
+     */
+    public function __construct(
+        private PaymentRequestFactoryInterface $paymentRequestFactory,
+        private PaymentRequestAnnouncerInterface $paymentRequestAnnouncer,
+        private ServiceProviderAwareProviderInterface $httpResponseProvider,
+        private PaymentRequestRepositoryInterface $paymentRequestRepository,
+        private PaymentStateFlashHandlerInterface $paymentStateFlashHandler,
+        private FinalUrlProviderInterface $orderPayFinalUrlProvider,
+    ) {
+    }
+
+    public function getResponse(RequestConfiguration $requestConfiguration): Response
+    {
+        $hash = $this->getPaymentRequestHash($requestConfiguration);
+        Assert::notNull($hash, 'A request attribute "hash" is required to retrieve the related order.');
+
+        $paymentRequest = $this->paymentRequestRepository->find($hash);
+        if (null === $paymentRequest) {
+            throw new NotFoundHttpException(sprintf('The Payment Request with hash "%s" does not exist.', $hash));
+        }
+
+        $statusPaymentRequest = $this->paymentRequestFactory->createFromPaymentRequest($paymentRequest);
+        $statusPaymentRequest->setAction(PaymentRequestInterface::ACTION_STATUS);
+
+        $this->paymentRequestAnnouncer->dispatchPaymentRequestCommand($statusPaymentRequest);
+
+        if ($this->httpResponseProvider->supports($requestConfiguration, $statusPaymentRequest)) {
+            return $this->httpResponseProvider->getResponse($requestConfiguration, $statusPaymentRequest);
+        }
+
+        /** @var PaymentInterface $payment */
+        $payment = $paymentRequest->getPayment();
+        $this->paymentStateFlashHandler->handle($requestConfiguration, $payment->getState());
+
+        $url = $this->orderPayFinalUrlProvider->getUrl($payment);
+
+        return new RedirectResponse($url);
+    }
+
+    public function supports(RequestConfiguration $requestConfiguration): bool
+    {
+        $hash = $this->getPaymentRequestHash($requestConfiguration);
+
+        return null !== $hash;
+    }
+
+    private function getPaymentRequestHash(RequestConfiguration $requestConfiguration): mixed
+    {
+        return $requestConfiguration->getRequest()->attributes->get('hash');
+    }
+}
---
+++ OrderPay/Provider/PaymentRequestPayResponseProvider.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\OrderPay\Provider;
+
+use Sylius\Bundle\CoreBundle\OrderPay\Resolver\PaymentToPayResolverInterface;
+use Sylius\Bundle\CoreBundle\PaymentRequest\Announcer\PaymentRequestAnnouncerInterface;
+use Sylius\Bundle\CoreBundle\PaymentRequest\Provider\ServiceProviderAwareProviderInterface;
+use Sylius\Bundle\ResourceBundle\Controller\RequestConfiguration;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Payment\Factory\PaymentRequestFactoryInterface;
+use Sylius\Component\Payment\Model\PaymentRequestInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Webmozart\Assert\Assert;
+
+final class PaymentRequestPayResponseProvider implements PayResponseProviderInterface
+{
+    /**
+     * @param PaymentRequestFactoryInterface<PaymentRequestInterface> $paymentRequestFactory
+     */
+    public function __construct(
+        private PaymentRequestFactoryInterface $paymentRequestFactory,
+        private PaymentRequestAnnouncerInterface $paymentRequestAnnouncer,
+        private ServiceProviderAwareProviderInterface $httpResponseProvider,
+        private RouterInterface $router,
+        private PaymentToPayResolverInterface $paymentToPayResolver,
+    ) {
+    }
+
+    public function getResponse(
+        RequestConfiguration $requestConfiguration,
+        OrderInterface $order
+    ): Response {
+        $payment = $this->paymentToPayResolver->getLastPayment($order);
+        Assert::notNull($payment, sprintf('Order (id %s) must have last payment in state "new".', $order->getId()));
+
+        $paymentMethod = $payment->getMethod();
+        Assert::notNull($paymentMethod, sprintf('Payment (id %s) must have payment method.', $payment->getId()));
+
+        $paymentRequest = $this->paymentRequestFactory->create($payment, $paymentMethod);
+
+        $this->paymentRequestAnnouncer->dispatchPaymentRequestCommand($paymentRequest);
+
+        if ($this->httpResponseProvider->supports($requestConfiguration, $paymentRequest)) {
+            return $this->httpResponseProvider->getResponse($requestConfiguration, $paymentRequest);
+        }
+
+        $url = $this->router->generate('sylius_shop_order_after_pay', [
+            'hash' => $paymentRequest->getId(),
+        ]);
+
+        return new RedirectResponse($url);
+    }
+
+    public function supports(
+        RequestConfiguration $requestConfiguration,
+        OrderInterface $order
+    ): bool {
+        return true;
+    }
+}
