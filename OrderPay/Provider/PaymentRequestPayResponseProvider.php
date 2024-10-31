1a5ee0c19f311d68134995ec663bfb4a74af5239
Francis Hilaire <Prometee@users.noreply.github.com>
2024-10-31 17:29:26 +0100

    Check if the retrieved payment request is not already finalized

--- OrderPay/Provider/PaymentRequestPayResponseProvider.php
+++ OrderPay/Provider/PaymentRequestPayResponseProvider.php
@@ -14,11 +14,14 @@ declare(strict_types=1);
 namespace Sylius\Bundle\CoreBundle\OrderPay\Provider;
 
 use Sylius\Bundle\CoreBundle\OrderPay\Resolver\PaymentToPayResolverInterface;
+use Sylius\Bundle\PaymentBundle\Checker\FinalizedPaymentRequestCheckerInterface;
 use Sylius\Bundle\PaymentBundle\Provider\DefaultActionProviderInterface;
 use Sylius\Bundle\PaymentBundle\Provider\DefaultPayloadProviderInterface;
 use Sylius\Bundle\ResourceBundle\Controller\RequestConfiguration;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Payment\Factory\PaymentRequestFactoryInterface;
+use Sylius\Component\Payment\Model\PaymentInterface;
+use Sylius\Component\Payment\Model\PaymentMethodInterface;
 use Sylius\Component\Payment\Model\PaymentRequestInterface;
 use Sylius\Component\Payment\Repository\PaymentRequestRepositoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -37,6 +40,7 @@ final class PaymentRequestPayResponseProvider implements PayResponseProviderInte
         private PaymentRequestRepositoryInterface $paymentRequestRepository,
         private DefaultActionProviderInterface $defaultActionProvider,
         private DefaultPayloadProviderInterface $defaultPayloadProvider,
+        private FinalizedPaymentRequestCheckerInterface $finalizedPaymentRequestChecker,
         private PaymentToPayResolverInterface $paymentToPayResolver,
         private UrlProviderInterface $paymentRequestPayUrlProvider,
     ) {
@@ -50,6 +54,23 @@ final class PaymentRequestPayResponseProvider implements PayResponseProviderInte
         $paymentMethod = $payment->getMethod();
         Assert::notNull($paymentMethod, sprintf('Payment (id %s) must have payment method.', $payment->getId()));
 
+        $paymentRequest = $this->getPaymentRequest($payment, $paymentMethod);
+        if (null === $paymentRequest->getHash()) {
+            $this->paymentRequestRepository->add($paymentRequest);
+        }
+
+        return new RedirectResponse($this->paymentRequestPayUrlProvider->getUrl($paymentRequest));
+    }
+
+    public function supports(RequestConfiguration $requestConfiguration, OrderInterface $order): bool
+    {
+        return true;
+    }
+
+    private function getPaymentRequest(
+        PaymentInterface $payment,
+        PaymentMethodInterface $paymentMethod
+    ): PaymentRequestInterface {
         $paymentRequest = $this->paymentRequestFactory->create($payment, $paymentMethod);
         $action = $this->defaultActionProvider->getAction($paymentRequest);
         $paymentRequest->setAction($action);
@@ -60,18 +81,11 @@ final class PaymentRequestPayResponseProvider implements PayResponseProviderInte
             $paymentMethod
         );
 
-        if (null === $existingPaymentRequest) {
+        if (null === $existingPaymentRequest || $this->finalizedPaymentRequestChecker->isFinal($existingPaymentRequest)) {
             $paymentRequest->setPayload($this->defaultPayloadProvider->getPayload($paymentRequest));
-            $this->paymentRequestRepository->add($paymentRequest);
-        } else {
-            $paymentRequest = $existingPaymentRequest;
+            return $paymentRequest;
         }
 
-        return new RedirectResponse($this->paymentRequestPayUrlProvider->getUrl($paymentRequest));
-    }
-
-    public function supports(RequestConfiguration $requestConfiguration, OrderInterface $order): bool
-    {
-        return true;
+        return $existingPaymentRequest;
     }
 }
