667610f4996aeb7b325ada0cf74e371ae05d1a57
Grzegorz Sadowski <sadowskigp@gmail.com>
2024-11-07 15:16:45 +0100

    Change application's version to v2.0.0-DEV

--- SyliusCoreBundle.php
+++ SyliusCoreBundle.php
@@ -40,7 +40,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class SyliusCoreBundle extends AbstractResourceBundle
 {
-    public const VERSION = '2.0.0-RC.1';
+    public const VERSION = '2.0.0-DEV';
 
     public const VERSION_ID = '20000';
 
@@ -50,7 +50,7 @@ final class SyliusCoreBundle extends AbstractResourceBundle
 
     public const RELEASE_VERSION = '0';
 
-    public const EXTRA_VERSION = 'RC.1';
+    public const EXTRA_VERSION = 'DEV';
 
     /** @return string[] */
     public function getSupportedDrivers(): array
