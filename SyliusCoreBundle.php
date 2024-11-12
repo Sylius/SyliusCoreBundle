8caa63e7e66454e94573c8962a9eb567a33cee70
Grzegorz Sadowski <sadowskigp@gmail.com>
2024-11-12 17:44:24 +0100

    Fix application's version v2.0.1-DEV

--- SyliusCoreBundle.php
+++ SyliusCoreBundle.php
@@ -40,17 +40,17 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class SyliusCoreBundle extends AbstractResourceBundle
 {
-    public const VERSION = '2.0.0';
+    public const VERSION = '2.0.1-DEV';
 
-    public const VERSION_ID = '20000';
+    public const VERSION_ID = '20001';
 
     public const MAJOR_VERSION = '2';
 
     public const MINOR_VERSION = '0';
 
-    public const RELEASE_VERSION = '0';
+    public const RELEASE_VERSION = '1';
 
-    public const EXTRA_VERSION = '';
+    public const EXTRA_VERSION = 'DEV';
 
     /** @return string[] */
     public function getSupportedDrivers(): array
